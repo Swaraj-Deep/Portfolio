e6938a20e701c61867acf4bf904a18c2bc03b14b
Swaraj Deep <deep.swaraj7@gmail.com>
2021-08-12 01:06:41 +0530

    Add button, card and image-box components. Add Login service.<M-F11>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { ButtonComponent } from './components/button/button.component';
+import { ImageBoxComponent } from './components/image-box/image-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ButtonComponent,
+    ImageBoxComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/image-box/image-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-box',
+  templateUrl: './image-box.component.html',
+  styleUrls: ['./image-box.component.css']
+})
+export class ImageBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
