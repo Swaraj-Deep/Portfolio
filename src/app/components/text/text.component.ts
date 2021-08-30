4176056898f547a19fad02834f8e777903427ad6
Swaraj Deep <deep.swaraj7@gmail.com>
2021-08-30 14:52:02 +0530

    Add typewriter effect

--- src/app/components/text/text.component.ts
+++ src/app/components/text/text.component.ts
@@ -1,18 +1,46 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-text',
   templateUrl: './text.component.html',
   styleUrls: ['./text.component.css']
 })
-export class TextComponent implements OnInit {
+export class TextComponent implements AfterViewInit {
 
-  @Input() text: string = '';
+  @Input() text: string = 'Provide Text Here';
+  @Input() greeting: string = '';
+  @Input() words: string[] = ['Software Developer'];
+
+  @ViewChild('HTMLTypingElement', {
+    static: false,
+    read: ElementRef
+  }) htmlTypingElement !: ElementRef<HTMLHeadingElement>;
 
   constructor() {
   }
 
-  ngOnInit(): void {
+  private type(words: string[], typingElement: HTMLHeadingElement, count: number = 0, index: number = 0, currText: string = '',
+               currLetter: string = '') {
+    if (count === words.length) {
+      count = 0;
+    }
+    currText = words[count];
+    currLetter = currText.slice(0, ++index);
+    typingElement.textContent = currLetter;
+    if (currLetter.length === currText.length) {
+      count++;
+      index = 0;
+    }
+    setTimeout(() => {
+      this.type(words, typingElement, count, index, currText, currLetter);
+    }, 500);
+  }
+
+  ngAfterViewInit(): void {
+    if (!this.text) {
+      const typingElement = this.htmlTypingElement.nativeElement;
+      this.type(this.words, typingElement);
+    }
   }
 
 }
