745370895b961fedd3171a60fd99b7c1045cd4c0
Swaraj Deep <deep.swaraj7@gmail.com>
2021-09-16 02:43:39 +0530

    Add test file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,25 @@
 import {TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {HeaderComponent} from "./components/header/header.component";
+import {ExperienceComponent} from "./components/experience/experience.component";
+import {HomeComponent} from "./components/home/home.component";
+import {TextComponent} from "./components/text/text.component";
+import {ImageBoxComponent} from "./components/image-box/image-box.component";
+import {HamburgerComponent} from "./components/hamburger/hamburger.component";
+import {LogoComponent} from "./components/logo/logo.component";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        ExperienceComponent,
+        HomeComponent,
+        TextComponent,
+        ImageBoxComponent,
+        HamburgerComponent,
+        LogoComponent
       ],
     }).compileComponents();
   });
