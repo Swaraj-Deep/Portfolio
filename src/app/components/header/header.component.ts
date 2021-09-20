76e5c0591c53904012e43be7e1f432a665d5747d
Swaraj Deep <deep.swaraj7@gmail.com>
2021-09-21 01:16:01 +0530

    Add on load active class

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Output} from '@angular/core';
 import {ImageBoxConfig} from "../../models/ImageBox.model";
 import {Link} from "../../models/Link.model";
 
@@ -7,7 +7,7 @@ import {Link} from "../../models/Link.model";
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
 
   @Output() onLinkClick: EventEmitter<string> = new EventEmitter<string>();
   configImgSrc: ImageBoxConfig = new ImageBoxConfig({height: '50px', width: '50px', borderRadius: '50%', zIndex: '10'});
@@ -27,7 +27,13 @@ export class HeaderComponent implements OnInit {
   constructor() {
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.leftLinks = this.leftLinks.map((link) => {
+      return {...link, isActive: window.location.hash.slice(1) === link.actualLink};
+    });
+    this.rightLinks = this.rightLinks.map((link) => {
+      return {...link, isActive: window.location.hash.slice(1) === link.actualLink};
+    });
   }
 
   toggleNavBar(): void {
--- src/app/models/Link.model.ts
+++ src/app/models/Link.model.ts
@@ -1,4 +1,5 @@
 export interface Link {
   toDisplay: string;
   actualLink: string;
+  isActive?: boolean;
 }
