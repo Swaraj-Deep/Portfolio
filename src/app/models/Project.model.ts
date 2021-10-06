41000ef5c16e2830bf006fcc027b1afae1961d39
Swaraj Deep <deep.swaraj7@gmail.com>
2021-10-07 02:16:56 +0530

    Add Projects section

---
+++ src/app/models/Project.model.ts
@@ -0,0 +1,9 @@
+export interface Project {
+  projectName: string;
+  technologiesUsed: string[];
+  link: string;
+  description: string;
+  futureScope: string[];
+  startDate: Date;
+  endDate?: Date;
+}
